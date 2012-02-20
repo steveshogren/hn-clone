1a4078a603d6c777e4efb40f33c74b122a726fcd
Steve <madklowns@gmail.com>
2012-02-20 17:48:06 -0500

    passing functions

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+exports.start = function () {
+    function onRequest(request, response) {
+	console.log("request recieved");
+	
+	  response.writeHead(200, {"Content-Type": "text/plain"});
+	  response.write("Hello World");
+	  response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("server started");
+}
