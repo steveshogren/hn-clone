2ab058816b756f67399afcb7d710cba03ec5410b
Steve <madklowns@gmail.com>
2012-02-20 18:39:07 -0500

    Passing in a route function to the server

---
+++ router.js
@@ -0,0 +1,3 @@
+exports.route = function (pathname) {
+    console.log("about to route for: " + pathname);
+}
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require("http");
+var url = require("url");
 
-exports.start = function () {
+exports.start = function (route) {
     function onRequest(request, response) {
-	console.log("request recieved");
+	var pathname = url.parse(request.url).pathname;
+	console.log("request for " + pathname + " recieved");
+
+	route(pathname);
 	
-	  response.writeHead(200, {"Content-Type": "text/plain"});
-	  response.write("Hello World");
-	  response.end();
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
     }
     http.createServer(onRequest).listen(8888);
     console.log("server started");
