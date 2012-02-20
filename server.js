87958f4b69977334b2ef3c1407edd12053c66833
Steve <madklowns@gmail.com>
2012-02-20 18:49:03 -0500

    Getting the info from the request

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var http = require("http");
 var url = require("url");
 
-exports.start = function (route) {
+function start(route, handle) {
     function onRequest(request, response) {
-	var pathname = url.parse(request.url).pathname;
-	console.log("request for " + pathname + " recieved");
+        var postData = "";
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
 
-	route(pathname);
-	
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+        request.setEncoding("utf8");
+
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '"+
+                postDataChunk + "'.");
+        });
+
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
+
     }
+
     http.createServer(onRequest).listen(8888);
-    console.log("server started");
+    console.log("Server has started.");
 }
+
+exports.start = start;
