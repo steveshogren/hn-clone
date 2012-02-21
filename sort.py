aaf6a7473acd7613dcc629dc6c25980edac60b1d
Steve Shogren <madklowns@gmail.com>
2012-02-21 14:08:16 -0500

    adding a python file to get the list of posts and display how old they are

---
+++ sort.py
@@ -0,0 +1,19 @@
+import redis
+from datetime import datetime
+from datetime import timedelta
+
+currentdate = datetime.now()
+print 'current date: ' + currentdate.isoformat()
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+for id in r.smembers('allPosts'):
+    votes = r.get('post:'+id+':votes')
+    date = r.get('post:'+id+':date')
+    if not votes: 
+        votes = '0'
+    if not date:
+        date = '0'
+    print ' date from redis: ' + date
+    date = datetime.fromtimestamp(int(date)/1000)
+    diff = currentdate - date
+    print 'id: ' + id + ' votes: ' + votes + ' date: ' + date.isoformat() + '   minutes since last used: ' + str(diff.seconds/60)
+    
