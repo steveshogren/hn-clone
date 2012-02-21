2a7b53b166956fd540017cc2ab3cc559a21a2eaf
Steve <madklowns@gmail.com>
2012-02-20 19:33:48 -0500

    Adding BASIC persistence with Redis

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,25 +1,25 @@
 var querystring = require("querystring"),
     fs = require("fs"),
-    formidable = require("formidable");
+    formidable = require("formidable"),
+    redis = require("redis");
 
 function start(response) {
     console.log("Request handler 'start' was called.");
 
-    var body = '<html>'+
-        '<head>'+
-        '<meta http-equiv="Content-Type" '+
-        'content="text/html; charset=UTF-8" />'+
-        '</head>'+
-        '<body>'+
-        '<form action="/upload" enctype="multipart/form-data" '+
-        'method="post">'+
-        '<input type="file" name="upload" multiple="multiple">'+
-        '<input type="submit" value="Upload file" />'+
-        '</form>'+
-        '</body>'+
+    var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
         '</html>';
 
-    response.writeHead(200, {"Content-Type": "text/html"});
+    response.writeHead(200, {"Content-Type":"text/html"});
     response.write(body);
     response.end();
 }
@@ -29,33 +29,33 @@ function upload(response, request) {
 
     var form = new formidable.IncomingForm();
     console.log("about to parse");
-    form.parse(request, function(error, fields, files) {
-        console.log("parsing done");
+    form.parse(request, function (error, fields, files) {
+        console.log("parsing done", fields);
 
-        /* Possible error on Windows systems:
-         tried to rename to an already existing file */
-        fs.rename(files.upload.path, "/tmp/test.png", function(err) {
-            if (err) {
-                fs.unlink("/tmp/test.png");
-                fs.rename(files.upload.path, "/tmp/test.png");
-            }
+        var client = redis.createClient();
+        client.stream.on("connect", function () {
+            client.incr('nextPost', function (err, id) {
+                client.set('post:' + id, JSON.stringify(fields.text), function () {
+                    var message = 'The post has been saved at <a href="/' + id + '">' + request.headers.host + '/' + id + '</a>';
+                    response.writeHead(200, {"Content-Type":"text/html"});
+                    response.write("received message:<br/>");
+                    response.write(message);
+                    response.end();
+                })
+            })
         });
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("received image:<br/>");
-        response.write("<img src='/show' />");
-        response.end();
     });
 }
 
 function show(response) {
     console.log("Request handler 'show' was called.");
-    fs.readFile("/tmp/test.png", "binary", function(error, file) {
-        if(error) {
-            response.writeHead(500, {"Content-Type": "text/plain"});
+    fs.readFile("/tmp/test.png", "binary", function (error, file) {
+        if (error) {
+            response.writeHead(500, {"Content-Type":"text/plain"});
             response.write(error + "\n");
             response.end();
         } else {
-            response.writeHead(200, {"Content-Type": "image/png"});
+            response.writeHead(200, {"Content-Type":"image/png"});
             response.write(file, "binary");
             response.end();
         }
